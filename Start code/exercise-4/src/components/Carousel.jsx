427b21c2d3f83e0f02cbcc96ef3c69db75e7f1b8
mean1718 <meansoklenh2708@gmail.com>
2026-03-10 15:55:13 +0700

    ex4

--- Start code/exercise-4/src/components/Carousel.jsx
+++ Start code/exercise-4/src/components/Carousel.jsx
@@ -7,32 +7,29 @@ export const Carousel = ({ images }) => {
   /* You will need to hanle the click on left and right button */
   const prevImage = () => {
     const newIndex = currentIndex === 0 ? images.length - 1 : currentIndex - 1;
+
     setCurrentIndex(newIndex);
-    /* You will need to manage the cases when we are on the last image or first image*/
-    const nextImage = () => {
-      const newIndex =
-        currentIndex === images.length - 1 ? 0 : currentIndex + 1;
-      setCurrentIndex(newIndex);
-    };
-    return (
-      <div className="carousel">
-        <BsArrowLeftCircleFill
-          className="arrow arrow-left"
-          onClick={prevImage}
-        />
-
-        {/* YOu will need to display the current image, not the first one.. */}
-        <img
-          src={images[currentIndex].src}
-          alt={images[currentIndex].alt}
-          className="slide"
-        />
-
-        <BsArrowRightCircleFill
-          className="arrow arrow-right"
-          onClick={nextImage}
-        />
-      </div>
-    );
   };
+
+  /* You will need to manage the cases when we are on the last image or first image*/
+  const nextImage = () => {
+    const newIndex = currentIndex === images.length - 1 ? 0 : currentIndex + 1;
+
+    setCurrentIndex(newIndex);
+  };
+  return (
+    <div className="carousel">
+      <BsArrowLeftCircleFill className="arrow arrow-left" onClick={prevImage} />
+      /* YOu will need to display the current image, not the first one.. */
+      <img
+        src={images[currentIndex].src}
+        alt={images[currentIndex].alt}
+        className="slide"
+      />
+      <BsArrowRightCircleFill
+        className="arrow arrow-right"
+        onClick={nextImage}
+      />
+    </div>
+  );
 };
